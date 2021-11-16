3626471f6ca70a736e19fee55e06b8453dba2f35
Bri <brigabdsl@hotmail.com>
2021-11-16 19:13:03 +0100

    Rate limiter implementation

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -5,11 +5,12 @@ const router = express.Router(); //création router = usage "router." et non "ap
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 const userCtrl = require('../controllers/user-controllers');
+const rateLimit = require('../middlewares/rate-limit');
 
 /* Route post: création compte */
-router.post('/signup', userCtrl.signup);
+router.post('/signup', rateLimit.Limiter, userCtrl.signup);
 /* Route post: authentification */
-router.post('/login', userCtrl.login);
+router.post('/login', rateLimit.Limiter, userCtrl.login);
 /* Route get: récupération un user */
 router.get('/:id', auth, userCtrl.getOneUser)
 /* Route get: récupération tous les users */
