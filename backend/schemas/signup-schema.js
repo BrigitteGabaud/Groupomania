92c9358a7def4c7c0df0d610f59017994268b077
Bri <brigabdsl@hotmail.com>
2021-11-16 18:36:15 +0100

    Creation of login-schema and signup-schema / corrections

--- backend/schemas/signup-schema.js
+++ backend/schemas/signup-schema.js
@@ -4,13 +4,27 @@ const joi = require('joi');
 
 // Construction schema validation email + pasword
 const schemaSignup = joi.object({
+    firstname: joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+    lastname: joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
     email: joi.string()
     .email({minDomainSegments:2, tlds:{allow:["com", "fr", "net"]}})
     .required()
     .pattern(new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)),
-    password:joi.string()
+
+    password: joi.string()
     .min(8)
-    .required()
+    .required(),
+
 });
 
 module.exports = schemaSignup;
