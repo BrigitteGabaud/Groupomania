b2db94daf6f0559ece63d35a733443ef21b88f11
Bri <brigabdsl@hotmail.com>
2021-11-27 06:24:04 +0100

    firsts routes implementations

---
+++ frontend/groupomania/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '@/views/Login.vue'
+//import Profile from '@/views/Profile.vue'
+
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+/*   {
+    path: '/Profile',
+    name: 'Profile',
+    component: Profile
+  } */
+  //{
+   // path: '/about',
+    //name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //}
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
