06af65ed06ede3940f59df75ae6f9e009fd2b55b
Bri <brigabdsl@hotmail.com>
2021-12-09 14:52:22 +0100

    Basic implementation of postList

--- frontend/groupomania/src/components/Posts/PostsList.vue
+++ frontend/groupomania/src/components/Posts/PostsList.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="container mt-5">
+    <div  class="container-post">
         <ul class="card">
-            <li v-for="post in posts" :key="post.id">
-                <div class="card">
-                    {{ post }}
+            <li v-for="post in listPosts" :key="post.id">
+                <div
+                 class="card">
+                 <h1>{{ post.id}}</h1>
+                    <p>{{ post.content }}</p>
+                    <p>{{ post.description}}</p>
+                    <div>
+                        <button class="btn btn-primary col-4 ">Commenter</button>
+                        <button class="btn btn-primary col-4">Supprimer</button>
+                    </div>
+                    
                 </div>
             </li>
-
-        </ul>
         
-    </div>
+        </ul>
     
+        
+    
+    </div>
 </template>
 
 <script>
@@ -18,41 +27,95 @@
 
     export default {
         name: "PostsList",
-        data: function() {
-            return {
-                posts: {
-                    id: '',
-                    content: '',
-                    description: ''
+      
+        data () {
+            return{
+                listPosts: [],
+                userInfos: {
+                    useId :'',
+                    firstname:'',
+                    lastname: '',
+                    email: '',
+                    password: '',
+                    role: '',
+                    avatar: '',
+                    bio: ''
+                },
+                post: {
+                    id: ''
                 }
             }
         },
-        components: {
-        
-        },
-        methods: {
-            getAllPosts: function() {
-                const post = {
-                    content: this.content,
-                    description: this.description,
-                }
-                 axios
-                    .get("http://localhost:3000/api/post", post)
-                    .then(response => {
-                        console.log(response);
-                        this.posts = response.data.url
-                    }) 
-                    .catch(error => {
-                        console.log(error.response);
-                    })
 
-            }
+        methods: {
+            getAllPosts: function () {
+                // return axios.post('http://localhost:3000/api/post/', {data})
+                // return axios.get('http://localhost:3000/api/post/'+ `${id}`)
+                return axios.get('http://localhost:3000/api/post').then(
+                    (res) => {
+                    
+                        this.listPosts = res.data;
+                    }
+                )
+            },
+             deletePost: function() {
+                let user = sessionStorage.getItem('user');
+                console.log('user local storage fron postlist' ,user);
+                user = JSON.parse(user);
+                console.log('user P', user);
+                return axios.delete('http://localhost:3000/api/post' + `${id}` ) 
+                .then((res) =>{
+                    this.id = res.data.id
+                    console.log(res.data);
+                })
+            } 
+            /* getInfosOfUser: function() {
+                this.$store.dispatch('getInfosOfUser', {
+                    firstname:this.firstname,
+                    lastname: this.lastname,
+                    email: this.email,
+                    password: this.password,
+                    role: this.role,
+                    avatar: this.avatar,
+                    bio: this.bio, 
+                }).then(function(response) {
+                    console.log(response)
+                }).catch(function(error) {
+                    console.log(error);
+                })
+            }, */
+        },
+        beforemount() {
+            /* const getInfosOfUser = this.store.state.userInfos
+            console.log('getInfosOfUser', getInfosOfUser); */
+        },
+        mounted() {
+            
+           this.getAllPosts()
+           //this.getInfosOfUser()
+           this.deletePost()
         }
     
     }
 </script>
 
-<style>
 
+<style scoped>
+.container-post {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 100px;
+}
+ul li {
+    list-style-type: none;
+    margin-bottom: 5px;
+}
+.btn  {
+    margin-right: 1px!important;
+}
 
 </style>
+
+A
