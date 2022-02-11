fe91ccfd96d772287131e498dac48dd27c70e5c7
Bri <brigabdsl@hotmail.com>
2022-02-11 10:27:47 +0100

    Responsive header and refactoring

--- frontend/groupomania/src/components/TheHeader.vue
+++ frontend/groupomania/src/components/TheHeader.vue
@@ -2,33 +2,61 @@
 
     <div class="container-header ">
 
-        <nav class="navbar .navbar-expand-md">
+        <nav class="navbar navbar-expand-md">
 
-            <li class="nav-item active ">
-                <router-link to="/">Accueil</router-link>
-            </li>
+            <img
+                class="navbar-brand"
+                id="logo"
+                src="@/assets/icon-left-font-monochrome-black_opttt.png" alt="Logo Groupomania">
 
-            <li v-if="!user"
-                @click="toggleLogin" :toggleLogin="toggleLogin">
-                <router-link to="/Connexion">Connexion</router-link>
 
-            </li>
 
-            <li v-else @click="logout" class="logout"> 
-                Déconnexion
-            </li>
+            <button
+                class="navbar-toggler "
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#nav-list"
+                aria-expanded="false"
+                aria-label="Navigation petits écrans">
 
-            <li >
-                <router-link to="/Profile">Profil</router-link>
-            </li>
+                <img src="../assets/hamburger.svg" class="navbar-toggler-icon">
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="nav-list">
+
+                <ul class="navbar-nav">
+
+                    <li class="nav-item active ">
+                        <router-link to="/">Accueil </router-link>
+                    </li>
+
+                    <li class="nav-item" >
+                        <router-link to="/Profile">Profil</router-link>
+                    </li>
+
+                    <li class="nav-item"
+                        v-if="!user"
+                        @click="toggleLogin" :toggleLogin="toggleLogin">
+                        <router-link to="/Connexion">Connexion </router-link>
+
+                    </li>
+
+                    <li v-else @click="logout" class="nav-item logout">
+                        Déconnexion
+                    </li>
+
+
+                </ul>
+
+            </div>
 
         </nav>
 
-        <login 
-        :reveleLogin="reveleLogin" :toggleLogin="toggleLogin" ></login>   
-        
+        <login :reveleLogin="reveleLogin" :toggleLogin="toggleLogin" ></login>
+
     </div>
-    
+
 </template>
 
 <script>
@@ -50,12 +78,9 @@ export default {
         toggleLogin: function() {
             this.reveleLogin = !this.reveleLogin
         },
-        toggleSignup: function() {
-            this.reveleSignup = !this.reveleSignup
-        },
         isUserLoggedIn: function() {
             let user = sessionStorage.getItem('user');
-            
+
             if(user) {
                 this.user = JSON.parse(user);
                 this.user == true
@@ -63,16 +88,25 @@ export default {
                 this.user == false
             }
         },
+        toggleHamburger: function() {
+            const menu = document.querySelector('.nav-list');
+            const btnMenu = document.querySelector('btn-toggle-container')
+
+            btnMenu.addEventListener('click', function() {
+                menu.classList.toggle('active-menu')
+            })
+        },
         logout: function() {
         this.$store.commit('logout');
         this.$router.push('/Connexion');
+        window.location.href = 'http://localhost:8080/connexion'
       }
     },
-    
+
     mounted() {
         this.isUserLoggedIn()
-    }   
- 
+    }
+
 }
 </script>
 
@@ -80,41 +114,86 @@ export default {
 .container-header {
     position: fixed;
     top: 0;
-    height: 50px;
-    background: gray;
-    color: black;
     width: 100%;
-    text-align: center;
-    line-height: 50px;
-    font-size: 20px;
-
+    height: 70px;
+    background:rgba(209,81,90,0.9);
+    color: #000000;
+    z-index: 5;
 }
-.button {
-    padding:8px;
-    border: none;
-    border-radius: 8px;
-    background:#a5a3a3!important;
-    font-weight: 500!important;
-    font-size: 16px;
-    flex:1;
-    min-width: 100px;
-} 
 .navbar {
-    display: flex;
-    justify-content: center!important;
-    list-style-type: none;
-    
+    justify-content: space-between;
 }
-li {
-    margin-right: 20px;
+img#logo.navbar-brand {
+    padding-bottom: 0;
+    margin-right: 0;
+    height: 3rem;
+    margin-left: 5%;
 }
-li a {
+#nav-list {
+    background:rgba(209,81,90,0.9);
+    margin-top: 4.5%;
+    padding-left: 4%;
+    padding-bottom: 6%;
+    font-weight: 500;
+}
+.nav-item {
+    margin: 0 10px;
+}
+.nav-item a  {
     text-decoration: none;
     color: black;
+    font-size: 1rem;
 }
 .logout {
     cursor: pointer;
+    font-size: 1rem;
+}
+button:focus {
+    outline: none!important;
+    box-shadow: none;
 }
 
+/* Ecrans mobiles taille moyenne */
+@media (min-width: 375px ) and (max-width: 397px){
+    #nav-list {
+        margin-top: 3.8%;
+    }
+}
+
+@media (min-width: 398px ) and (max-width: 425px){
+    #nav-list {
+        margin-top: 3%;
+    }
+}
+
+/* Ecrans tablettes */
+@media (min-width: 768px) {
+    .navbar-expand-md .navbar-collapse {
+        justify-content: flex-end;
+        margin-left: 5%;
+        height: 70px;
+    }
+    #nav-list {
+        background: none;
+        margin-right: 5%;
+        margin-top: 0.3rem;
+        padding: 0;
+        font-weight: 500;
+    }
+    .navbar {
+        height: 70px;
+    }
+    img#logo.navbar-brand {
+        height: 4rem;
+        margin-left: 5%;
+    }
+    .nav-item a  {
+        font-size: 1.2rem;
+    }
+    .logout {
+        cursor: pointer;
+        font-size: 1.2rem;
+    }
+}
 
 </style>
