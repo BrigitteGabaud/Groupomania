27618c85bb03acb6ea02633bbb9860362dc9005a
Bri <brigabdsl@hotmail.com>
2022-02-21 15:43:27 +0100

    add comments

--- frontend/groupomania/src/router/index.js
+++ frontend/groupomania/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// Pages
 import Home from '@/views/Home.vue'
 import Profile from '@/views/Profile.vue'
 import Connexion from '@/views/Connexion.vue'
 import Admin from '@/views/Admin.vue'
 
 
-
+// Création du routes
 const routes = [
   {
     path: '/',
@@ -29,9 +31,11 @@ const routes = [
   }
 ]
 
+// Création du router
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
+// Exportation du router
 export default router
