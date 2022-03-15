90c77e4636c518285fb270d4d64458513a4c19d2
Bri <brigabdsl@hotmail.com>
2022-03-15 11:16:05 +0100

    Add: .getAllPostsOfOneUser  Fix: multer

--- backend/routes/post-routes.js
+++ backend/routes/post-routes.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /* Import des dépendances  et fonctions */
 const express = require('express');
 const router = express.Router(); //création router = usage "router." et non "app."
@@ -7,13 +8,11 @@ const multer = require('../middlewares/multer-config');
 const postCtrl = require('../controllers/post-controllers');
 
 /* Routes post */
+router.get('/user/:userId', auth, postCtrl.getAllPostsOfOneUser)
+router.get('/', auth, postCtrl.getAllPosts);
 router.post('/', auth, multer,  postCtrl.createPost);
-router.get('/', auth, multer, postCtrl.getAllPosts);
-router.get('/:id', auth, multer, postCtrl.getOnePost);
+router.get('/:id', auth, postCtrl.getOnePost);
 router.put('/:id', auth, multer, postCtrl.modifyPost);
-router.delete('/:id', auth, multer, postCtrl.deletePost)
-
-
-
+router.delete('/:id', auth, postCtrl.deletePost)
 
 module.exports = router;
