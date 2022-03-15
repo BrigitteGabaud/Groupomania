d901feeaa4b39d62d4f29293a4e15fc4b2e87c83
Bri <brigabdsl@hotmail.com>
2022-03-15 11:09:13 +0100

    Modify: modify / delete
    Add: userModifySchema

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -7,10 +7,13 @@ const fs = require('fs'); // Donne accès aux opérations liées aux syst de fic
 /* Import schemas joi */
 const schemaAuth = require('../schemas/signup-schema');
 const schemaLogin = require('../schemas/login-schema');
+const schemaUserModify = require('../schemas/userModify-schema');
+
 
 
 /* Import dossier models */
 const db = require('../models');
+const { log } = require('console');
 
 /*** Export de la fonction inscription ***/
 exports.signup = async (req, res) => {
@@ -59,7 +62,7 @@ exports.login = async (req, res)=> {
       }
     /* cherche user dans db par son ad mail */
     const user = await db.users.findOne({ where:{ email: req.body.email }})
-    console.log(user);
+    console.log('USER',user);
     /* si elle n'y est pas*/    
     if(!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !'}); 
@@ -67,7 +70,7 @@ exports.login = async (req, res)=> {
     /* Compare mdp req à celui db */
     bcrypt.compare(req.body.password, user.password)
     .then(valid => {
-      console.log(user);
+      console.log('USER 2',user);
       // si différent
         if (!valid) {
           return res.status(401).json({ error: 'Mot de passe incorrect !'});
@@ -103,88 +106,156 @@ exports.getAllUsers = (req, res) => {
 exports.getOneUser = (req, res) => {
   db.users.findOne({where :{ id: req.params.id }}) // = cherche ds db l'user dont l'id correspond
   .then(userResponse => res.status(200).json(userResponse))
-  .catch(error => res.status(400).json({ error }));
+  .catch(error => res.status(400).json({ error: error.message }));
 };
 
 
 /*** Export de la fonction MODIFICATION user (PUT) ***/
-exports.modifyUser = (req, res) => {
-  // Récupère l'id de l'utilisateur effectuant la requête
-  const reqUserId = req.user.userId;
-  console.log('reqUserId:', reqUserId);
+exports.modifyUser = async (req, res) => {
+  try {
+    /* Vérifie si la requête correspond au schemaUserModify */
+    const joiValidator = await schemaUserModify.validateAsync(req.body)
+    if(!joiValidator){
+      return res.status(400).json({ error: 'Modification impossible certaines données sont incorrectes' })
+    }
+   
+    const newUser = req.body;
+    console.log('newUser',  newUser);
+    // Récupère l'id de l'utilisateur effectuant la requête
+    const reqUserId = req.user.userId;
+    console.log('reqUserId:',   reqUserId);
+    
+    if(req.file) {
+      newUser.avatar = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    }
+
+    // Cherche l'user concerné dans la BDD 
+    db.users.findOne({where :{id: req.params.id}}) 
+    .then(User => {
+      console.log('userResponse : ' , User);
   
-  // Cherche l'user concerné dans la BDD 
-  db.users.findOne({where :{id: req.params.id}}) 
-  .then(userResponse => {
-    console.log('userResponse : ' ,userResponse);
-      // Si l'userId de la BDD correspond à celui de la requête
-      if(userResponse.id == reqUserId ) {
-        console.log('userResponse.id :' , userResponse.id);
-          /* L'user est modifié */
-          // Y a t-il une requête pour un fichier image ? 
-          const userObject = req.file ?
-          {
-          // Récupère toutes les informations du user
-          ...JSON.parse(req.body.user), 
-          // Génère l'image url
-          avatar: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-          // = Si req.file n'existe pas: on prend le corps de la requête
-          } : { ...req.body}; 
-          if(userResponse.avatar != 'http://localhost:3000/images/Bestiole_jaune.jpg') {
-            // récupère le segment après "image" et le supprime pour ne pas stocker les images
-            const fileName = userResponse.avatar.split("/images/")[1]
-            fs.unlink(`images/${fileName}`, (err) => {
-              err? console.log(err) : console.log("image supprimée");
-            })
-          }
-          // modifie l'identifiant de l'objet créé
-          userResponse.update({...userObject})
-          .then(() => res.status(200).json({ message: 'User modifié !'}))
-          .catch(error => res.status(400).json({error: error.message }));
-      } else {
-          /*Sinon envoie un message d'erreur et arrête le code (return)*/
-          return res.status(403).json({message:  `403: unauthorized request `})
-      }
-          
-  })
-  .catch(error => res.status(500).json({ error: error.message }))
+      if(!User) {
+        return res.status(404).json({ error: "Utilisateur non trouvé !" })
+      } 
+      if (User.role !== "admin" && User.id !== reqUserId){
+        console.log(User.id !== reqUserId);
+        return res.status(401).json({ error: "L'utilisateur ne dispose pas des droits pour effectuer cette modification" })
+      }  
+      
+      if(req.file){
+
+        if(User.avatar) {
+            console.log('USER AVATAR?', User.avatar);
+          const filename = User.avatar.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+
+            // Puis modifie le user
+            User.update({...newUser})
+            .then(() => res.status(200).json({ message: 'User modifié !'}))
+            .catch(error => res.status(400).json({error: error.message || `Une erreur s'est produite pendant la modification du user` }));
+          })}
+
+          console.log('NEW USER IMG', newUser);
+      } else { 
+
+        // Modifie le contenu texte
+        User.update({...newUser})
+        .then(() => res.status(200).json({ message: 'User modifié !'}))
+        .catch(error => res.status(400).json({error: error.message || `Une erreur s'est produite pendant la modification du User` }));
+        console.log('NEW USER ', newUser);
+      }    
+
+
+   
+    }) 
+    .catch(error => res.status(500).json({ error: error.message }))
+  }
+  catch(err){
+    return res.status(500).json({ err: err.message })
+  }
 }
 
 /*** Export de la fonction SUPPRESSION user (DELETE) ***/
-// to do : penser à suppr post etc
 exports.deleteUser = (req, res) => {
+  const reqUser = req.user;
+  console.log('REQ USER', reqUser);
   const reqUserId = req.user.userId;
   console.log('reqUserId:', reqUserId);
   const id = req.params.id;
+  console.log('REQ PARAMS ID', id);
 
   db.users.findByPk(id) 
   .then(userResponse => {
-   
-    // Si l'userId de la BDD correspond à celui de la requête
-    if(userResponse.id == reqUserId ) {
-          console.log('userResponse.id :', userResponse.id);
-      // La sauce est supprimée
-      /* Accède à l'objet pour récup url image + nom fichier */
-      db.users.findOne({ where: { id : req.params.id }}) 
-      /* callback récupère sauce + nom exact fichier */
-      .then(user => {
-        console.log('USER: ' ,user);
-        console.log('USER avatar :' , user.avatar);
-          /* Extrait nom du fichier à suppr :Récupère imageUrl sauce retournée / base + split autour '/images/' et retourne tableau de 2 elmts avant/ après /images/ */
-          const filename = user.avatar.split('/images/')[1];
-          /* Fonction suppression fichier: 1er arg= chemin fichier 2e = callback (que faire une fois fichier suppr ? = suppr objet dans la base) */
-          fs.unlink(`images/${filename}`, (err) => {
-            err ? console.log(err) : console.log( 'Image supprimée');
-          })
-          db.users.destroy({ where: { id : req.params.id }}) 
-            .then(() => res.status(200).json({ message:'User supprimé !'}))
-            .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ message: error.message }));
-    } else {
-      // Sinon renvoie une erreur et stoppe le code
-      return res.status(401).json({message: '401: unauthorized request' })
+    if(! userResponse) {
+      return res.status(404).json({message: `Utilisateur non trouvé` })
     }
+    // Si l'userId de la BDD est différent de celui de la requête
+    if(userResponse.id !== reqUserId ) {
+      console.log('userResponse.id :', userResponse.id);
+      return res.status(401).json({message: `1 L'utilisateur n'est pas autorisé à effectuer cette action.` })
+    }
+    if (userResponse.userRole !== reqUser.role) {
+      return res.status(403).json({ message: `2 L'utilisateur n'est pas pas autorisé à effectuer cette action.` })
+    }
+
+    /* Accède à l'objet pour récup url image + nom fichier */
+    db.users.findOne({ where: { id : req.params.id }}) 
+    
+    .then(User => {
+      console.log('USER: ' ,User);
+      console.log('USER avatar :' , User.avatar);
+      console.log(('USER ID', User.id));
+      console.log(User.role !== "admin");
+      console.log( reqUserId !== User.id);
+      console.log(reqUserId);
+      
+      if (User.role !== "admin" && reqUserId !== User.id) {
+        return res.status(403).json({ message: `3 L'utilisateur n'est pas pas autorisé à effectuer cette action.` })
+      } else return db.posts.findAll({ where : { userId: reqUserId} })
+    })
+
+    .then(comments => {
+      if(comments == "") {
+        return db.posts.findAll({ where: {userId: reqUserId}})
+      } else {
+        for (let comment in comments) {
+          comments[comment].destroy()
+        }
+        return db.posts.findAll({ where: {userId: reqUserId}})
+      }
+    })
+
+    .then(posts => {
+      if(posts == "") {
+        return db.users.findAll({ where: {id: reqUserId}})
+      } else {
+        for (let post in posts) {
+          if(posts[post].image) {
+            const filename = post[post].image.split("/images/")[1];
+            fs.unlink(`images/${filename}`, () => {
+              posts[post].destroy()
+            })
+          } else {
+            post[post].destroy()
+          }
+        }
+        return db.users.findOne({ where: {id: reqUserId}})
+      }
+    })
+
+    .then(user => {
+      console.log('USERRR', user[0].avatar);
+      const User = user[0]
+      const avatar = user[0].avatar
+      console.log('AVATAR', avatar);
+      if(user.avatar !== 'http://localhost:3000/images/Bestiole_jaune.jpg') {
+        const filename = avatar.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+        User.destroy()
+        return res.status(200).json({message: "Utilisateur supprimé !"})
+        })
+      }
+    })
   })
   .catch(error => res.status(500).json({ message: error.message }));
 }
