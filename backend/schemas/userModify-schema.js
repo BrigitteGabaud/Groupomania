8de89cd32b4b4a5ca3d2ed4b9acb86fde617e086
Bri <brigabdsl@hotmail.com>
2022-03-15 11:19:11 +0100

    Build

---
+++ backend/schemas/userModify-schema.js
@@ -0,0 +1,33 @@
+"use strict";
+// Import Joi
+const joi = require('joi');
+const joiPassword = require("joi-password");
+
+// Construction schema validation email + password
+const schemaUserModify = 
+
+joi
+
+.object({
+    firstname: joi.string()
+    .alphanum()
+    .min(3)
+    .max(30),
+
+    lastname: joi.string()
+    .alphanum()
+    .min(3)
+    .max(30),
+
+    email: joi.string()
+    .email({minDomainSegments:2, tlds:{allow:["com", "fr", "net"]}})
+    .pattern(new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)),
+
+    bio: joi.string()
+    .max(255)
+    .regex(/^.{1,50}$/)
+})
+
+
+module.exports = schemaUserModify;
+
