12270dcd1db952a43482573edbff84725296958d
Bri <brigabdsl@hotmail.com>
2022-03-15 11:31:27 +0100

    Add: getters / getUserInfos / sUserConnected + refactor

--- frontend/groupomania/src/store/index.js
+++ frontend/groupomania/src/store/index.js
@@ -1,11 +1,9 @@
-import { createStore } from 'vuex'
-const axios = require('axios').default;
-const { DateTime } = require("luxon");
-DateTime.now().toString();
-let dt = DateTime.now();
-let currentLocaleDate = dt.setLocale('fr').toLocaleString(DateTime.DATETIME_FULL);
-console.log('currentLocaleDate', currentLocaleDate);
+import { createStore, createLogger } from 'vuex'
+import axios from 'axios'
+import router from '../router/index';
+//const axios = require('axios').default;
 
+// Configuration axios
 const instance = axios.create({
   baseURL: "http://localhost:3000/api"
 })
@@ -14,20 +12,21 @@ const instance = axios.create({
 const defaultUser = {
   userId: -1,
   token: '',
-  
+  role: ''
 }
 
 // Local Storage
-let user = sessionStorage.getItem('user');
-console.log('user local storage' ,user);
+let user = localStorage.getItem('user');
+console.log('user:', user)
 
 if (!user) {
  user = defaultUser;
+ console.log('user2:', user)
 } else {
   try {
     user = JSON.parse(user); // récupère user depuis local storage
     instance.defaults.headers.common['Authorization'] = user.token; // défini header autorisation pour utiliser token
-    console.log(('user:' , user));
+    
   } catch (ex) {
     console.log('erreur recup' ,ex.data);
     user = defaultUser;
@@ -40,12 +39,8 @@ if (!user) {
     webSiteName: 'Groupomania',
     status: '', // status de base
     user: user,
-    post: {
-      id: '',
-      content: '',
-      description: ''
-    },
-    userInfos: {
+    userInfos: [{
+      id:'',
       firstname:'',
       lastname: '',
       email: '',
@@ -53,69 +48,149 @@ if (!user) {
       role: '',
       avatar: '',
       bio: ''
+    }],
+    postInfos: [{
+      id: '',
+      content: '',
+      image: '',
+    }]
+  },
+  getters: {
+    fullName: (state) => {
+      return `${state.userInfos.firstname} ${state.userInfos.lastname}`
     },
+    copyright: (state) => {
+			const currentYear = new Date().getFullYear()
+			return `© ${state.webSiteName} ${currentYear}`
+		}
   },
   mutations: {
-    setStatus: function(state, status) { // Défini le status
+    
+    SET_STATUS(state, status) { // Défini le status
       state.status= status;
     },
-    logUser:function(state, user) { // user loggé
+    LOG_USER(state, user) { // user loggé
       instance.defaults.headers.common['Authorization'] = user.token; // quand logué récupère token dans header
-      sessionStorage.setItem('user', JSON.stringify(user));// sauvegarde token
-      console.log('mut' ,sessionStorage.setItem('user', JSON.stringify(user)));
+      localStorage.setItem('user', JSON.stringify(user));// sauvegarde token
       state.user = user;
     },
-    userInfos: function (state, userInfos) { // récupère infos user
+    USER_INFOS(state, userInfos) { // récupère infos user
       state.userInfos = userInfos;
+      console.log('userinfos depuis mut', state.userInfos);
+    },
+    EDIT_FIRST_NAME(state, value) {
+      state.userInfos.firstname = value
     }, 
-    logout: function(state) {
+    EDIT_LAST_NAME(state, value) {
+      state.userInfos.lastname = value
+    }, 
+    EDIT_AVATAR(state, value) {
+      state.userInfos.avatar = value
+    }, 
+    EDIT_BIO(state, value) {
+      state.userInfos.bio = value
+    }, 
+    EDIT_ROLE(state, value) {
+      state.userInfos.role = value
+    }, 
+    POST_INFOS: function (state, postInfos) { // récupère infos post
+      state.postInfos = postInfos;
+    }, 
+    LOGOUT: function(state) {
       state.user = defaultUser;
-      sessionStorage.removeItem('user');
+      localStorage.removeItem('user');
     }
 
   },
   actions: {
+    /**
+     * @description Fonction permettant la connexion du user
+     */
     login: ({commit}, userInfos) => {
-      commit('setStatus', 'loading'); // défini status en mode loading
+      
+      commit('SET_STATUS', 'loading'); // défini status en mode loading
       return new Promise((resolve, reject) => {
-      instance.post('/user/login', userInfos)
-      .then(function(response) {
-        commit('setStatus', ''); // = si logué
-        commit('logUser', response.data); // commit logUser + récupère données
-        resolve(response);
-      })
-      .catch(function(error) {
-        commit('setStatus', 'error_login'); // si erreur
-        reject(error);
-      })
+      instance.post('/user/login', userInfos) // instance = axios
+
+        .then(function(response) {
+          commit('SET_STATUS', ''); // = si logué
+          commit('LOG_USER', response.data); // commit LOG_USER + récupère données
+          resolve(response);
+        })
+        .catch(function(error) {
+          commit('SET_STATUS', 'error_login'); // si erreur
+          console.log(error.message)
+          reject(error.message);
+        
+        })
       })
     },
+
+    /**
+     * @description Fonction permettant l'inscription du user
+     */
     signup: ({commit}, userInfos) => { // 1er param: commit --> pour exec mut 
       
-      commit('setStatus', 'loading'); // défini status en mode loading
+      commit('SET_STATUS', 'loading'); // défini status en mode loading
       return new Promise((resolve, reject) => {
       instance.post('/user/signup', userInfos) // instance = ref baseURL L7 / appelle méthode signup ds login.vue
-      .then(function(response) {
-        commit('setStatus', 'created'); // = si compte créé
-        resolve(response);
-      })
-      .catch(function(error) {
-        commit('setStatus', 'error_create'); // = si erreur
-        reject(error);
-      })
+
+        .then(function(response) {
+          commit('SET_STATUS', 'created'); // = si compte créé
+          resolve(response);
+        })
+        .catch(function(error) {
+          commit('SET_STATUS', 'error_create'); // = si erreur
+          reject(error.message);
+        })
       })
     },
-/*       getInfosOfUser:({commit}, userInfos) => {
-      
-      instance.get(`/user/${userId}`)
-      .then(function(response) {
-        commit('userInfos', response.data);
+
+    /**
+     * @description Fonction actualisant le user à partir des userInfos
+     */
+    getUserInfos({ commit }) {
+      //console.log(localStorage.getItem('user'));
+      axios({
+        method: "get",
+        url: `http://localhost:3000/api/user/${user.userId}`,
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": ` Bearer ${user.token}`
+        }
       })
-      .catch(function(error) {
-        commit('userInfos', error.message);
+
+      .then(user => {
+        commit("EDIT_FIRST_NAME", user.data.firstname)
+        commit("EDIT_LAST_NAME", user.data.lastname)
+        commit("EDIT_AVATAR", user.data.avatar)
+        commit("EDIT_BIO", user.data.bio)
+        commit("EDIT_ROLE", user.data.role)
       })
-    },    */
-  }
+      .catch(error => {
+        console.log(error.response)
+        localStorage.clear()
+        router.push({ name: "Connexion"})
+      })
+    },
+
+    
+    /**
+     * @description Fonction vérifiant si l'user est connecté
+     */
+    isUserConnected() {
+      if(!user.token || !user.userId) {
+        console.log('user token from index',user.token);
+        console.log(user.userId);
+        router.push({ name: "Connexion"})
+      }
+    }
+  },
+
+
+  // Logger valable en mode developpement
+  plugins:  [createLogger()]
 })
 
-export default store;
+// Exportation du store
+export default store
