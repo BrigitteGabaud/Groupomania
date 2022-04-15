c2b5e53c93f4052c194640f5fbcaf80789f68e77
Bri <brigabdsl@hotmail.com>
2022-04-15 19:49:15 +0200

    Modify: rate limit max

--- backend/middlewares/rate-limit.js
+++ backend/middlewares/rate-limit.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 /* Fonction limitation requêtes création compte */
 exports.Limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 1 heure de pénalisation
-    max: 100, // Bloque à partir de 5 requêtes
+    max: 15, // Bloque à partir de 15 requêtes
     message:
       "Trop de tentatives de connexion ou de création de comptes à partir de cette adresse IP, veuillez réessayer d'ici une heure."
 });
